36d1b22fb033ae0d90a475939b25a6265c446d92
elouazzani yassine <80113396+yassine121212@users.noreply.github.com>
2022-08-25 00:57:48 +0100

    init

--- firebase.js
+++ firebase.js
@@ -3,7 +3,12 @@ import firebase from "firebase";
 // GET YOURSELF A FIREBASE CONFIG
 
 const firebaseConfig = {
-  // PAST HERE
+  apiKey: "AIzaSyAVN_8EeWLdVI1gzG521GmK0O539h4_qhg",
+  authDomain: "whatsapp-3f188.firebaseapp.com",
+  projectId: "whatsapp-3f188",
+  storageBucket: "whatsapp-3f188.appspot.com",
+  messagingSenderId: "705032609631",
+  appId: "1:705032609631:web:4313863a6c05b3c462e0ea",
 };
 
 const app = !firebase.apps.length
